d481e27588a415e7b1e093af1aa8e8ce39ab7bdd
Soe Moe Tun <67118878+soemoehtun@users.noreply.github.com>
2025-10-18 05:28:15 +0630

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.getElementById("lookupBtn").addEventListener("click", async () => {
         }
 
         // Wait 1500ms (1.5 seconds) between each request to avoid rate limit
-        await delay(3000);
+        await delay(4500);
     }
 
     alert("Done!");
